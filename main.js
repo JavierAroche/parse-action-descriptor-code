0dfdddb6f26892993f5b84eaddc5a7f91f16e9b4
Javier Aroche <aroche290@gmail.com>
2017-11-14 23:59:55 -0800

    fix(): fixed space error when cleaning up variables.
    add(): initial implementation of createFunction

--- main.js
+++ main.js
@@ -41,17 +41,24 @@ function cleanJSX(data) {
 	var lines = data.split('\n');
 	var variables = {};
 	var actionLines = [];
+	// Identify lines with charIDs or stringIDs
 	lines.forEach(function(line) {
 		if (line.match('var') && (line.match('charIDToTypeID') || line.match('stringIDToTypeID'))) {
 			var variableName = line.replace(/[\s+]*var /, '').replace(/ =.+/, '');
 			var id = line.replace(/[\s+]*.+= /, '').replace(';', '');
 			variables[variableName] = id;
 		} else {
-			var cleanLine = line.replace(/\s+/, '');
+			var cleanLine;
+			if(line[0] === ' ' || line[0] === '    ') {
+				cleanLine = line.replace(/\s+/, '');
+			} else {
+				cleanLine = line;
+			}
 			actionLines.push(cleanLine);
 		}
 	});
 
+	// Cleanup lines
 	var parsedLines = [];
 	actionLines.forEach(function(actionLine) {
 		var idNames = actionLine.match(/id\w+/g);
@@ -77,7 +84,10 @@ function sortIDs(data) {
 			var id = line.replace(/[\s+]*.+= /, '').replace(';', '');
 			variables[variableName] = id;
 		} else {
-			var cleanLine = line.replace(/\s+/, '');
+			var cleanLine = line;
+			if(cleanLine[0] === ' ') {
+				cleanLine = cleanLine.replace(/\s+/, '');
+			}
 			actionLines.push(cleanLine);
 		}
 	});
@@ -91,28 +101,14 @@ function sortIDs(data) {
 }
 
 function createFunction(data) {
-	return data
-}
-
-function cleanDesc(data) {
 	var lines = data.split('\n');
 	var variables = {};
 	var actionLines = [];
-	var descCount = 1;
-	var refCount = 1;
 	lines.forEach(function(line) {
-		if (line.match('var') && (line.match('ActionDescriptor') || line.match('ActionReference'))) {
-			actionLines.push(line);
+		if (line.match(/desc\w+.put/)) {
 			var variableName = line.replace(/[\s+]*var /, '').replace(/ =.+/, '');
-			var newVariableName;
-			if(line.match('ActionDescriptor')) {
-				newVariableName = 'desc' + descCount;
-				descCount = descCount + 1;
-			} else {
-				newVariableName = 'ref' + refCount;
-				refCount = refCount + 1;
-			}
-			variables[variableName] = newVariableName;
+			var id = line.replace(/[\s+]*.+= /, '').replace(';', '');
+			variables[variableName] = id;
 		} else {
 			var cleanLine = line;
 			if(cleanLine[0] === ' ') {
@@ -124,8 +120,60 @@ function cleanDesc(data) {
 
 	var parsedLines = [];
 	actionLines.forEach(function(actionLine) {
-		var descVars = actionLine.match(/desc\w+/g);
+		var idNames = actionLine.match(/id\w+/g);
 		var parsedLine = actionLine;
+		if (idNames && idNames.length > 0) {
+			idNames.forEach(function(idName) {
+				parsedLine = parsedLine.replace(idName, variables[idName]);
+			});
+		}
+		parsedLines.push(parsedLine);
+	});
+
+	return parsedLines.join('\n');
+
+	return data
+}
+
+function cleanDesc(data) {
+	var lines = data.split('\n');
+	var variables = {};
+	var actionLines = [];
+	var descCount = 1;
+	var refCount = 1;
+	var listCount = 1;
+	// Identify lines with variables
+	lines.forEach(function(line) {
+		if (line.match('var') && (line.match('ActionDescriptor') || line.match('ActionReference') || line.match('ActionList'))) {
+			actionLines.push(line);
+			var variableName = line.replace(/[\s+]*var /, '').replace(/ =.+/, '');
+			var newVariableName;
+			if(line.match('ActionDescriptor')) {
+				newVariableName = 'desc' + descCount;
+				descCount = descCount + 1;
+			} else if(line.match('ActionReference')) {
+				newVariableName = 'ref' + refCount;
+				refCount = refCount + 1;
+			} else if(line.match('ActionList')) {
+				newVariableName = 'list' + listCount;
+				listCount = listCount + 1;
+			}
+			variables[variableName] = newVariableName;
+		} else {
+			var cleanLine = line;
+			if(cleanLine[0] === ' ') {
+				cleanLine = cleanLine.replace(/\s+/, '');
+			}
+			actionLines.push(cleanLine);
+		}
+	});
+
+	// Cleanup variable names
+	var parsedLines = [];
+	actionLines.forEach(function(actionLine) {
+		var parsedLine = actionLine;
+
+		var descVars = actionLine.match(/desc\w+/g);
 		if (descVars && descVars.length > 0) {
 			descVars.forEach(function(descVar) {
 				parsedLine = parsedLine.replace(descVar, variables[descVar]);
@@ -136,6 +184,13 @@ function cleanDesc(data) {
 		if (refVars && refVars.length > 0) {
 			refVars.forEach(function(refVar) {
 				parsedLine = parsedLine.replace(refVar, variables[refVar]);
+			});
+		}
+
+		var listVars = actionLine.match(/list\w+/g);
+		if (listVars && listVars.length > 0) {
+			listVars.forEach(function(listVar) {
+				parsedLine = parsedLine.replace(listVar, variables[listVar]);
 			});
 		}
 
